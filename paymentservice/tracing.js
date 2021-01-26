8b56d5b66fb364d3718983616de6f0ff5ff5bf4b
jw-cohen <11511251+jw-cohen@users.noreply.github.com>
2021-01-26 14:25:45 -0800

    Add initial src files

---
+++ paymentservice/tracing.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const { LogLevel } = require("@opentelemetry/core");
+const opentelemetry = require('@opentelemetry/api');
+const { NodeTracerProvider } = require("@opentelemetry/node");
+const { BasicTracerProvider, ConsoleSpanExporter, SimpleSpanProcessor } = require('@opentelemetry/tracing');
+const { CollectorTraceExporter } = require('@opentelemetry/exporter-collector');
+const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
+
+const provider = new NodeTracerProvider({
+  logLevel: LogLevel.ERROR
+});
+
+provider.addSpanProcessor(
+  new SimpleSpanProcessor(
+    new JaegerExporter({
+        serviceName: 'paymentservice',
+        endpoint: 'http://otel-agent:14268/api/traces',
+    })
+  )
+);
+
+//print traces to stdout for testing
+provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+
+provider.register();
+
+console.log("tracing initialized");
+
